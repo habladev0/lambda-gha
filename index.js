d7772c4a1e2041ff57f692e84314cc2b9d7a1758
habladev0 <info@habladev.com>
2024-01-07 11:17:21 -0500

    corrected message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Habla Dev from github actions!'),
+      body: JSON.stringify('Habla Dev from github actions for a second time!'),
     };
     return response;
   };
